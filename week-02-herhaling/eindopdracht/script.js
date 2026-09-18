38b133807e4c24c8ac3ddbd7d58b5174050ec92e
VinceVos9027799 <9027799@student.zadkine.nl>
2026-09-18 13:59:05 +0200

    Update script.js

--- week-02-herhaling/eindopdracht/script.js
+++ week-02-herhaling/eindopdracht/script.js
@@ -2,3 +2,30 @@
 // Stap 2: Luister naar het submit-event, lees de invoervelden uit met .value en toon het resultaat
 // Stap 3: Toon een foutmelding in #result als het bedrag of de korting leeg is
 // Bonus: Schrijf getKlantniveau(bedrag) en toon het niveau erbij
+
+
+const priceInput = document.querySelector('#amount');
+const discountInput = document.querySelector('#discount');
+const form = document.querySelector('#discount-form');
+const result = document.querySelector('#result');
+
+function calculateTotal(price, discount) {
+    return price - (price * discount / 100);
+}
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    if (priceInput.value === "" || discountInput.value === "") {
+        result.textContent = "Empty";
+        result.style.color = "red";
+        return;
+    }
+
+    const price = Number(priceInput.value);
+    const discount = Number(discountInput.value);
+
+    result.style.color = "black";
+    result.textContent = `value is ${calculateTotal(price, discount).toFixed(2)}`; 
+});
+
